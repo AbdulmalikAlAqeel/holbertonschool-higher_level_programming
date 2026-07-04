f04df973c3195c43941a042861c24577489dfc00
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-07-04 22:49:01 +0300

    Fix 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -17,11 +17,11 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         prev_row = triangle[i - 1]
-        
+
         # Calculate the internal values of the row
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-            
+
         row.append(1)  # The last element is always 1
         triangle.append(row)
 
